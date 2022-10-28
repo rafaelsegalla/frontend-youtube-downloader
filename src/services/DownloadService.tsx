e3b5d525a3bd1eb48b1c11687b29b681d0053090
Rafael Rodrigues Segalla <rrsegalla2000@gmail.com>
2022-10-28 20:21:24 -0300

    ajusta endpoint da api

--- src/services/DownloadService.tsx
+++ src/services/DownloadService.tsx
@@ -1,7 +1,7 @@
 export class DownloadService {
     static download(url: string|null) {
         const SERVER_URL = import.meta.env.VITE_SERVER_URI;
-        return fetch(`${SERVER_URL}?url=${url}`)
+        return fetch(`${SERVER_URL}/download?url=${url}`)
             .then(T => T.json())
     }
 }
